aca1e06933e779c0ab8b1d842366258c38fcbbf4
wasique <wumar42@gmail.com>
2025-03-15 23:37:21 +0530

    DataSci proj setup to use gemini with openAi compatibility

---
+++ back-services/ds-service/src/main/java/com/expense/ds_service/DsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.expense.ds_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsServiceApplication.class, args);
+	}
+
+}
---
+++ back-services/ds-service/src/test/java/com/expense/ds_service/DsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expense.ds_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
