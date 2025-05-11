1a7fac424a28d309720160962097366daf6c9e86
washub <42351950+washub@users.noreply.github.com>
2025-05-11 14:22:12 +0530

    Update DsServiceApplicationTests.java

--- back-services/ds-service/src/test/java/com/expense/ds_service/DsServiceApplicationTests.java
+++ back-services/ds-service/src/test/java/com/expense/ds_service/DsServiceApplicationTests.java
@@ -1,13 +1,21 @@
 package com.expense.ds_service;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class DsServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	@Test
+	void testHello(){
+		var msg = "Hello World";
+		Assertions.assertEquals("Hello World", msg);
 	}
 
 }
